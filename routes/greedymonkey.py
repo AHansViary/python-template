6f73071f4f78cc90baec59516218fa67d2f38dbb
AHansViary <120349420+AHansViary@users.noreply.github.com>
2023-09-23 11:41:47 +0800

    Update greedymonkey.py

--- routes/greedymonkey.py
+++ routes/greedymonkey.py
@@ -1,8 +1,7 @@
 import json
 import logging
-
+from time import time
 from flask import request
-
 from routes import app
 
 logger = logging.getLogger(__name__)
@@ -21,13 +20,15 @@ def greedy_monkey_solve(w, v, f):
 
 @app.route('/greedymonkey', methods=['POST'])
 def evaluate():
+    start = time()
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     w = data["w"]
     v = data["v"]
     f = data["f"]
+    finish = time() - start
     
     result = greedy_monkey_solve(w, v, f)
-    
+    logging.info("Time elapsed  :{}".format(finish))
     logging.info("My result :{}".format(result))
     return json.dumps(result)
