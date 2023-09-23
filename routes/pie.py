0edcffe8c6c100dd9757f8950d209e72e746cb2a
AHansViary <120349420+AHansViary@users.noreply.github.com>
2023-09-23 23:24:55 +0800

    Update pie.py

--- routes/pie.py
+++ routes/pie.py
@@ -2,7 +2,6 @@ from flask import request
 import math
 import json
 import logging
-
 from routes import app
 logger = logging.getLogger(__name__)
 
@@ -20,14 +19,19 @@ def generate_pie_chart(instruments):
     total_values = sum(sorted_values)
     proportions = [(q / total_values) for q in sorted_values]
     
-    # Find if there is a proportion less than 0.05
-    # if it is, increase it to 0.05 and decrease the next one for adjustment
-    if any(p < 0.05 for p in proportions):
+    # Find if there is a proportion less than 0.05%
+    # if it is, increase it to 0.05% and decrease the one before for adjustment
+    if any(p < 0.0005 for p in proportions):
         for i in range(len(proportions)):
-            if proportions[i] < 0.05:
-                proportions[i] = 0.05
-                proportions[i+1] -= 0.05 - proportions[i]
+            if proportions[i] < 0.0005:
+                # Do proportional adjustment
+                dif = 0.0005- proportions[i]
+                proportions[i] = 0.0005
+                for j in range(i):
+                    proportions[j] -= dif*proportions[j]
+                    
     # Convert the proportions to radian
+    print(proportions)
     radian = [p * 2 *math.pi for p in proportions]
     
     # Return the radians in summed up list
@@ -36,7 +40,7 @@ def generate_pie_chart(instruments):
         radian_summed.append(sum(radian[:i+1]))
     # max 7 decimal points or 8?
     radian_summed = [round(r, 8) for r in radian_summed]
-    return radian_summed
+    return [round(a, 8) for a in radian_summed]
 
 @app.route('/pie-chart', methods=['POST'])
 def evaluate():
@@ -47,4 +51,3 @@ def evaluate():
     
     logging.info("My result :{}".format(result))
     return json.dumps(result)
-  
