53fa21ace2bffeaf860527fe47231d32d966c0d9
AHansViary <120349420+AHansViary@users.noreply.github.com>
2023-09-23 21:12:45 +0800

    Update and rename digital_colony.py to digitalcolony.py

--- routes/digitalcolony.py
+++ routes/digitalcolony.py
@@ -6,31 +6,31 @@ from routes import app
 logger = logging.getLogger(__name__)
 @app.route('/digital-colony', methods=['POST'])
 
-def calculate_weight(colony):
-    return sum(map(int,colony))
+def calculate_weight(c):
+    return sum(map(int,c))
 
 def calculate_signature(pair):
     digit1, digit2 = pair[0], pair[1]
     return (digit1 - digit2) % 10
 
-def generate_next_generation(colony):
-    weight = calculate_weight(colony)
+def generate_next_generation(d):
+    weight = calculate_weight(d)
     new_generation = []
-    for i in range(len(colony)-1):
-        pair = colony[i:i+2]
+    for i in range(len(d)-1):
+        pair = d[i:i+2]
         signature = calculate_signature(pair)
         new_digit = (weight + signature) % 10
-        new_generation.append(colony[i])
+        new_generation.append(d[i])
         new_generation.append(new_digit)
-    new_generation.append(colony[-1])
+    new_generation.append(d[-1])
     return new_generation
 
 def simulate_generations(start_colony, num_generations):
-    colony = [int(digit) for digit in start_colony]
+    e = [int(digit) for digit in start_colony]
     for _ in range(num_generations):
-        colony = generate_next_generation(colony)
-    total_weight = calculate_weight(colony)
-    return colony, total_weight
+        e = generate_next_generation(e)
+    total_weight = calculate_weight(e)
+    return e, total_weight
 
 def evaluate():
     start = time()
